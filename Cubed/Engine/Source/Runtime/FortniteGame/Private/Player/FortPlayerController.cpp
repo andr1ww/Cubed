501a7148289b5e40d1b2fc40862b560d6d2b2825
Itztiva <tiva@Itztiva.com>
2025-10-06 01:00:47 -0400

    ServerAcknowledgePossession & ServerExecuteInventoryItem

---
+++ Cubed/Engine/Source/Runtime/FortniteGame/Private/Player/FortPlayerController.cpp
@@ -0,0 +1,59 @@
+﻿#include "pch.h"
+#include "Engine/Source/Runtime/FortniteGame/Public/Player/FortPlayerController.h"
+
+#include "Offsets.h"
+#include "Engine/Plugins/HookingLibrary/Public/HookingLibrary.h"
+#include "Engine/Source/Runtime/CoreUObject/Public/Templates/Casts.h"
+
+void FortPlayerController::ServerAcknowledgePossession(AFortPlayerControllerAthena* Controller, APawn* Pawn)
+{
+    Controller->AcknowledgedPawn = Pawn;
+    UFortKismetLibrary::UpdatePlayerCustomCharacterPartsVisualization((AFortPlayerState*)Controller->PlayerState);
+    
+    Controller->XPComponent->bRegisteredWithQuestManager = true;
+    Controller->XPComponent->OnRep_bRegisteredWithQuestManager();
+    Controller->GetQuestManager(ESubGame::Athena)->InitializeQuestAbilities(Pawn);
+}
+
+void FortPlayerController::ServerExecuteInventoryItem(AFortPlayerController* Controller, FGuid ItemGuid)
+{
+    if (!Controller || !Controller->MyFortPawn) return;
+    FFortItemEntry* Entry = Controller->WorldInventory->Inventory.ReplicatedEntries.Search([&](FFortItemEntry& entry) {
+        return entry.ItemGuid == ItemGuid;
+    });
+    
+    if (Controller->MyFortPawn)
+    {
+        if (Entry->ItemDefinition->IsA(UFortDecoItemDefinition::StaticClass()))
+        {
+            auto DecoItemDefinition = Cast<UFortDecoItemDefinition>(Entry->ItemDefinition);
+            if (!DecoItemDefinition) return;
+    
+            Controller->MyFortPawn->PickUpActor(nullptr, DecoItemDefinition);
+            Controller->MyFortPawn->CurrentWeapon->ItemEntryGuid = ItemGuid;
+
+            if (auto ContextTrap = Cast<AFortDecoTool_ContextTrap>(Controller->MyFortPawn->CurrentWeapon))
+            {
+                ContextTrap->SetContextTrapItemDefinition(Cast<UFortContextTrapItemDefinition>(DecoItemDefinition));
+            }
+            return;
+        }
+   
+        Controller->MyFortPawn->EquipWeaponDefinition((UFortWeaponItemDefinition*)Entry->ItemDefinition, Entry->ItemGuid, Entry->TrackerGuid, false); 
+    }
+}
+
+void FortPlayerController::Setup()
+{
+    UHook* Hook = new UHook();
+
+    Hook->Address = Runtime::Vfts::ServerAcknowledgePossesion;
+    Hook->Class = AFortPlayerController::StaticClass();
+    Hook->Detour = ServerAcknowledgePossession;
+    UKismetHookingLibrary::Hook(Hook, EHook::EveryVFT);
+
+    Hook->Address = Runtime::Vfts::ServerExecuteInventoryItem;
+    Hook->Class = AFortPlayerController::StaticClass();
+    Hook->Detour = ServerExecuteInventoryItem;
+    UKismetHookingLibrary::Hook(Hook, EHook::EveryVFT);
+}
---
+++ Cubed/Engine/Source/Runtime/FortniteGame/Public/Player/FortPlayerController.h
@@ -0,0 +1,10 @@
+﻿#pragma once
+#include "pch.h"
+
+namespace FortPlayerController
+{
+    DefineOriginal(void, ServerAcknowledgePossession, AFortPlayerControllerAthena* Controller, APawn* Pawn);
+    DefineOriginal(void, ServerExecuteInventoryItem, AFortPlayerController* Controller, FGuid ItemGuid);
+    
+    void Setup();
+}
