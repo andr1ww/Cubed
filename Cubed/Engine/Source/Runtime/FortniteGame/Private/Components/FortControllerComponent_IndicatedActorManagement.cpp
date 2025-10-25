0b3e4b6aee28ce40cfee8748a4bdcf051718b28b
Itztiva <tiva@Itztiva.com>
2025-10-24 21:05:00 -0400

    stuff for flare guns yay

---
+++ Cubed/Engine/Source/Runtime/FortniteGame/Private/Components/FortControllerComponent_IndicatedActorManagement.cpp
@@ -0,0 +1,84 @@
+﻿#include "pch.h"
+#include "Engine/Source/Runtime/FortniteGame/Public/Components/FortControllerComponent_IndicatedActorManagement.h"
+
+#include "Engine/Plugins/HookingLibrary/Public/HookingLibrary.h"
+
+void FortControllerComponent_IndicatedActorManagement::Library::AddActorsToIndicatedList(UObject*, FFrame& Stack)
+{
+    AFortPlayerControllerAthena* InstigatingController;
+    TArray<AActor*>* IndicatedActors;
+    FIndicatedActorData IndicatedActorData;
+    bool bAddAsUnique;
+    bool bAllowOwningPlayer;
+    Stack.StepCompiledIn(&InstigatingController);
+    Stack.StepCompiledIn(&IndicatedActors);
+    Stack.StepCompiledIn(&IndicatedActorData);
+    Stack.StepCompiledIn(&bAddAsUnique);
+    Stack.StepCompiledIn(&bAllowOwningPlayer);
+    Stack.IncrementCode();
+
+    if (!InstigatingController) return;
+
+    auto Comp = (UFortControllerComponent_IndicatedActorManagement*)InstigatingController->GetComponentByClass(UFortControllerComponent_IndicatedActorManagement::StaticClass());
+    if (!Comp) return;
+
+    for (auto& Actor : *IndicatedActors)
+    {
+        if (!Actor) return;
+        FIndicatedActorInfoEntry Entry{};
+
+        Entry.Actor = Actor;
+        Entry.Data = IndicatedActorData;
+        Entry.EndTime = UGameplayStatics::GetTimeSeconds(GetWorld()) + 5.f;
+        Entry.StartTime = UGameplayStatics::GetTimeSeconds(GetWorld());
+        Entry.bRefreshExistingWhenAdded = true;
+
+        Comp->IndicatedActorList.Entries.Add(Entry);
+    }
+}
+
+void FortControllerComponent_IndicatedActorManagement::Library::AddActorsToStenciledList(UObject*, FFrame& Stack)
+{
+    AFortPlayerControllerAthena* InstigatingController;
+    TArray<class AActor*>* StenciledActors;
+    FStenciledActorData StenciledActorData;
+    bool bAddAsUnique;
+    Stack.StepCompiledIn(&InstigatingController);
+    Stack.StepCompiledIn(&StenciledActors);
+    Stack.StepCompiledIn(&StenciledActorData);
+    Stack.StepCompiledIn(&bAddAsUnique);
+    Stack.IncrementCode();
+    if (!InstigatingController) return;
+
+    auto Comp = (UFortControllerComponent_IndicatedActorManagement*)InstigatingController->GetComponentByClass(UFortControllerComponent_IndicatedActorManagement::StaticClass());
+    if (!Comp) return;
+
+    for (auto& Actor : *StenciledActors)
+    {
+        if (!Actor) return;
+        FStenciledActorInfoEntry Entry{};
+
+        Entry.Actor = Actor;
+        Entry.Data = StenciledActorData;
+        Entry.EndTime = UGameplayStatics::GetTimeSeconds(GetWorld()) + 5.f;
+        Entry.StartTime = UGameplayStatics::GetTimeSeconds(GetWorld());
+        Entry.bRefreshExistingWhenAdded = true;
+
+        Comp->StenciledActorList.Entries.Add(Entry);
+    }
+}
+
+void FortControllerComponent_IndicatedActorManagement::Setup()
+{
+    UHook* Hook = new UHook();
+
+    Hook->Path = "/Script/FortniteGame.FortniteGame.FortIndicatedActorManagementLibrary.AddActorsToIndicatedList";
+    Hook->Detour = Library::AddActorsToIndicatedList;
+    UKismetHookingLibrary::Hook(Hook, EHook::Exec);
+    
+    Hook->Path = "/Script/FortniteGame.FortIndicatedActorManagementLibrary.AddActorsToStenciledList";
+    Hook->Detour = Library::AddActorsToStenciledList;
+    UKismetHookingLibrary::Hook(Hook, EHook::Exec);
+
+    delete Hook;
+}
---
+++ Cubed/Engine/Source/Runtime/FortniteGame/Public/Components/FortControllerComponent_IndicatedActorManagement.h
@@ -0,0 +1,15 @@
+﻿#pragma once
+#include "pch.h"
+
+#include "Engine/Source/Runtime/CoreUObject/Public/UObject/Stack.h"
+
+namespace FortControllerComponent_IndicatedActorManagement
+{
+    namespace Library
+    {
+        static void AddActorsToIndicatedList(UObject*, FFrame& Stack);
+        static void AddActorsToStenciledList(UObject*, FFrame& Stack);
+    }
+    
+    void Setup();
+}
